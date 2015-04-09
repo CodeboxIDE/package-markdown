1c4d4d0501506f831bd00505781b4e044d3dde48
Samy Pessé <samypesse@gmail.com>
2015-04-09 10:00:31 +0200

    Adapt plugin for browserify

--- src/index.js
+++ src/index.js
@@ -1,23 +1,21 @@
-define([
-    "src/markdown",
-    "less!src/stylesheets/main.less"
-], function(markdown) {
-    var commands = codebox.require("core/commands");
-    var dialogs = codebox.require("utils/dialogs");
+require("./stylesheets/main.less");
+var marked = require("marked");
 
-    commands.register({
-        id: "markdown.preview",
-        title: "Markdown: Preview",
-        context: ["editor"],
-        run: function(args, context) {
-            return codebox.tabs.add(codebox.tabs.HtmlPanel, {
-                className: "component-markdown-preview",
-                content: markdown.render(context.getContent())
-            }, {
-                type: "markdown",
-                title: "Markdown: " + context.model.get("name"),
-                section: "markdown"
-            });
-        }
-    });
+var commands = codebox.require("core/commands");
+var dialogs = codebox.require("utils/dialogs");
+
+commands.register({
+    id: "markdown.preview",
+    title: "Markdown: Preview",
+    context: ["editor"],
+    run: function(args, context) {
+        return codebox.tabs.add(codebox.tabs.HtmlPanel, {
+            className: "component-markdown-preview",
+            content: marked(context.getContent())
+        }, {
+            type: "markdown",
+            title: "Markdown: " + context.model.get("name"),
+            section: "markdown"
+        });
+    }
 });
--- src/markdown.js
+++
@@ -1,7 +0,0 @@
-define([
-    "node_modules/marked/lib/marked"
-], function(marked) {
-    return {
-        render: function(s) { return marked(s); }
-    }
-});
